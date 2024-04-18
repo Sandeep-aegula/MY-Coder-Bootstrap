6c4d7efa64db16ad5164305da4b0bf0ab82ad856
Sandeep-aegula <167386724+Sandeep-aegula@users.noreply.github.com>
2024-04-18 19:18:47 +0530

    initial commit

---
+++ 111222333 pattren.c
@@ -0,0 +1,15 @@
+//237Y1A66B0
+#include<stdio.h>
+int main()
+{
+int i,j,n;
+printf ("enter n\n");
+scanf("%d",&n);
+for (i=1;i<=n;i++){
+for(j=1;j<=n;j++){
+ printf("%d",i);
+ }
+printf("\n");
+ }
+ return 0 ;
+ }
